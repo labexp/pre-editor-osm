0dd5b3500b793553b117fbbe4cc077aba33e3d50
Jaime Gutiérrez Alfaro <jaime.cr@gmail.com>
2021-03-23 11:52:28 -0600

    Añade argumento para imprimir mensajes que ayudan a depurar el código. Ajusta las opciones del análisis y se añaden métodos para visualizar la salidas

--- pre_editor.py
+++ pre_editor.py
@@ -15,6 +15,8 @@ parser.add_argument('--esquema', '-e', metavar='',
 parser.add_argument('--rango', '-r', metavar='', required=False, default=20,
                     type=int, help='radio en metros de la circunferencia donde se descargarán los '
                     + 'elementos de OSM')
+parser.add_argument('--debug', '-d', metavar='', required=False, default=False,
+                    type=bool, help='mostrar mensajes de depuración')
 
 args = parser.parse_args()
 
@@ -28,7 +30,7 @@ def parsear_esquema():
     """
     with open(args.esquema) as archivo_esquema:
         esquema_parseado = json.load(archivo_esquema)
-    
+
     return esquema_parseado
 
 
@@ -47,7 +49,7 @@ def mostrar_waypoints(gpx):
             print('\tNo hay nodos cerca del punto.')
 
 
-def descargar_nodos_en_rango(lat, lon):
+def descargar_nodos_en_rango(lat, lon, debug=False):
     """
     A partir de un punto (lat, lon) dado como argumento, descarga todos los nodos que se encuentren
     a una distancia de 'rango' metros.
@@ -58,6 +60,8 @@ def descargar_nodos_en_rango(lat, lon):
     # Consulta descarga nodos, pero si se cambia "node" por "nwr" descarga vías y relaciones.
     consulta = "node(around:%s, %s, %s); out;" % (args.rango, lat, lon)
     resultado = api.query(consulta)
+    if debug:
+        mostrar_nodos_descargados(resultado)
     return resultado
 
 
@@ -71,12 +75,13 @@ def mostrar_nodos_descargados(resultado, prefijo="\t", mostrar_etiquetas=True):
     ...
     [prefijo][prefijo]-etiquetaN- = -valorN-
     """
+    debug_preambulo = "DEBUG (mostrar_nodos_descargados): "
+    print(debug_preambulo + "Se descargaron " + str(len(resultado.nodes)) + " nodos.")
     for nodo in resultado.nodes:
-        print(prefijo, "https://osm.org/node/" + str(nodo.id))
+        print(debug_preambulo + prefijo + "https://osm.org/node/" + str(nodo.id))
         if mostrar_etiquetas:
             for etiqueta in nodo.tags:
-                print(prefijo*2 + etiqueta, ' = ' + nodo.tags[etiqueta])
-
+                print(debug_preambulo + prefijo*2 + etiqueta, ' = ' + nodo.tags[etiqueta])
 
 def imprimir_encabezado():
     """
@@ -87,6 +92,15 @@ def imprimir_encabezado():
     """
     msj = "Informe \n{0}\n\nAnalizando la ruta {1} con el esquema de mapeo {2}."
     print(msj.format("="*8, args.gpx, args.esquema))
+
+def imprimir_encabezado_waypoint(wpt_nombre, wpt_lat, wpt_lon):
+    """
+    Muestra en pantalla un encabezado correspondiente a un informe del análisis de un waypoiont.
+
+    No devuelve ningun valor.
+    """
+    msj = "\n\nAnalizando el waypoint con nombre '{0}' ubicado en {1},{2}."
+    print(msj.format(wpt_nombre, wpt_lat, wpt_lon))
 
 
 def imprimir_crear(latitud, longitud, etiquetas_nuevas):
@@ -138,7 +152,7 @@ def imprimir_revisar(id, etiquetas_sobrantes):
 def imprimir_resultado(nodo,waypoint,estructura_analisis):
     """
     Recibe una estructura de anális retormada por analizar_etiquetas y los datos del nodo y
-    waypoint. Con base a esto decide cuál resultado debe ser impreso. 
+    waypoint. Con base a esto decide cuál resultado debe ser impreso.
 
     No devuelve ningun valor.
     """
@@ -150,55 +164,103 @@ def imprimir_resultado(nodo,waypoint,estructura_analisis):
         imprimir_editar(nodo.id,etiquetas_analizadas)
     elif caso == "CREAR":
         imprimir_crear(waypoint.latitude,waypoint.longitude,etiquetas_analizadas)
-    else:
+    elif caso == "INFO":
         imprimir_info(nodo.id)
-        
-def analizar_traza(ruta_gpx):
+    else:
+        print("Nada por hacer")
+
+
+def analizar_traza(ruta_gpx, debug=False):
     """
     Recibe una ruta a un archivo GPX el cual cada uno de sus waypoints debe ser analizado.
 
-    No devuelve ningún valor
+    No devuelve ningún valor. Imprime en pantalla los resultados del análisis apoyándose en las
+    funciones auxiliares.
     """
-    archivo_gpx = open(args.gpx, 'r')
+    archivo_gpx = open(ruta_gpx, 'r')
     gpx = gpxpy.parse(archivo_gpx)
     # parsear esquema antes
     for waypoint in gpx.waypoints:
-        nodos_cercanos = descargar_nodos_en_rango(waypoint.lat,waypoint.lon)
+        nodos_cercanos = descargar_nodos_en_rango(waypoint.latitude,waypoint.longitude, debug)
         nombre_waypoint = waypoint.name
+        imprimir_encabezado_waypoint(nombre_waypoint, waypoint.latitude, waypoint.longitude)
         etiquetas_esquema = esquema_mapeo_global[nombre_waypoint]
-        for nodo in nodos_cercanos:     
+        resultados_analisis = list()
+        for nodo in nodos_cercanos.nodes:
             etiquetas_osm = nodo.tags
-            estructura_analisis = analizar_etiquetas(etiquetas_osm, etiquetas_esquema)
-            imprimir_resultado(nodo,waypoint,estructura_analisis)
-              
-def analizar_etiquetas(etiquetas_osm: dict, etiquetas_esquema: dict) -> list:
+            estructura_analisis = analizar_etiquetas(etiquetas_osm, etiquetas_esquema, debug)
+            if estructura_analisis[0] != "NADA":
+                resultados_analisis.append(estructura_analisis)
+                imprimir_resultado(nodo,waypoint,estructura_analisis)
+
+        if resultados_analisis == []:
+            # CREAR
+            imprimir_resultado(nodo,waypoint,["CREAR", etiquetas_esquema])
+
+    archivo_gpx.close()
+
+
+def analizar_etiquetas(etiquetas_osm: dict, etiquetas_esquema: dict,debug=True) -> list:
     """
     Segun las etiquetas en osm cercanas y las etiquetas en el esquema de cierto waypoint,
-    analiza si las etiquetas de osm se encuentran correctamente mapeadas o viceversa.
-    
-    Devuelve una estructura una lista con el formato
-    [cod_caso,etiquetas_sobrantes||faltantes||necesarias||null]
+    si las ambos dicciones son iguales (INFO), si hay al menos una coinciddncia en una etiqueta pero
+    hay ḿas etiquetas en el esquema (EDITAR), si hay condicen todas las etiquetas entre ambos
+    diccionario pero hay algunas adicionales en OSM (REVISAR). Si ninguno de los tres casos está
+    presente entonces se devuelve el código de caso NADA con etiqueta nulas.
+
+    Devuelve una estructura una lista con el formato:
+
+    [codigo_caso, etiquetas]
+
+    etiquetas será una opción entre: etiquetas_sobrantes||faltantes||necesarias||null
     """
+
+    debug_preambulo = "DEBUG (analizar_etiquetas): "
+
+    if debug:
+        print("\n\n")
+        print(debug_preambulo + "etiquetas_osm: " + str(etiquetas_osm))
+        print(debug_preambulo + "etiquetas_esquema: " + str(etiquetas_esquema))
     total_osm = len(etiquetas_osm)
     total_esquema = len(etiquetas_esquema)
 
     coincidencias = dict(etiquetas_osm.items() & etiquetas_esquema.items())
-    faltantes = dict(etiquetas_osm.items() - etiquetas_esquema.items())
-    sobrantes = dict(etiquetas_esquema.items() - etiquetas_osm.items())
+    # "fsobrantes_en_osm": están en OSM y no en el esquema.
+    sobrantes_en_osm = dict(etiquetas_osm.items() - etiquetas_esquema.items())
+    # sobrantes_esquema: están en el esquema y no en OSM
+    sobrantes_esquema = dict(etiquetas_esquema.items() - etiquetas_osm.items())
 
+    # INFO
     if total_esquema == total_osm and total_osm == len(coincidencias):
         # caso info si etiquetas_osm y etiquetas_esquema son iguales
+        if debug:
+            print(debug_preambulo + "INFO" + str(None))
         return ["INFO", None]
-    elif len(faltantes) >= len(coincidencias):
+
+    # EDITAR
+    elif len(coincidencias) >= 1 and len(sobrantes_esquema) >= 1:
         # caso editar si etiquetas_osm son mas que etiquetas_esquema
-        return ["EDITAR",faltantes]
-    elif len(coincidencias) == 0:
-        # caso crear si etiquetas_osm y etiquetas_esquema no coinciden
-        return ["CREAR",etiquetas_esquema]
-    elif len(sobrantes) >= len(coincidencias):
-        # caso crear si etiquetas_esquema  son mas que etiquetas_osm
-        return ["REVISAR",sobrantes]
-    
+        if debug:
+            print(debug_preambulo + "EDITAR" + str(sobrantes_esquema))
+        return ["EDITAR", sobrantes_esquema]
+
+    # REVISAR
+    elif len(coincidencias) == total_esquema and len(sobrantes_en_osm) > 0:
+        # caso crear si etiquetas_esquema son mas que etiquetas_osm
+        if debug:
+            print(debug_preambulo + "REVISAR" + str(sobrantes_en_osm))
+        return ["REVISAR", sobrantes_en_osm]
+
+    # NADA: candidato a caso de CREAR
+    else:
+        if debug:
+            print(debug_preambulo + "CASO NO CONTEMPLADO")
+            print(debug_preambulo + "total_osm: " + str(total_osm))
+            print(debug_preambulo + "total_esquema: " + str(total_esquema))
+            print(debug_preambulo + "coincidencias " + str(coincidencias))
+            print(debug_preambulo + "sobrantes_en_osm: " + str(sobrantes_en_osm))
+            print(debug_preambulo + "sobrantes_esquema: " + str(sobrantes_esquema))
+        return ["NADA", None]
 
 esquema_mapeo_global = parsear_esquema()
 
@@ -210,6 +272,22 @@ if __name__ == "__main__":
     mostrar_waypoints(gpx)
     mostrar_esquema(args.esquema)
     '''
-    etiquetas_osm = {'amenity':'taxi'}
-    etiquetas_esquema = {'amenity':'taxi'}
-    print(analizar_etiquetas(etiquetas_osm, etiquetas_esquema))
+    ruta_gpx = args.gpx
+    debug = args.debug
+
+    #caso info
+    #etiquetas_osm = {'amenity':'taxi'}
+    #etiquetas_esquema = {'amenity':'taxi'}
+    #print(analizar_etiquetas(etiquetas_osm, etiquetas_esquema))
+
+    # caso editar
+    #etiquetas_osm ={'crossing': 'traffic_signals', 'highway': 'traffic_signals', 'traffic_signals': 'crossing'}
+    #etiquetas_esquema = {'crossing': 'traffic_signals', 'traffic_signals:sound': 'walk;yes'}
+    #print(analizar_etiquetas(etiquetas_osm, etiquetas_esquema))
+
+    # caso revisar
+    #etiquetas_osm = {'bench': 'yes', 'bus': 'yes', 'covered': 'yes', 'highway': 'bus_stop', 'name': 'UNADECA', 'public_transport': 'platform', 'shelter': 'yes'}
+    #etiquetas_esquema = {'public_transport': 'platform', 'bench': 'yes', 'shelter': 'yes', 'bus': 'yes'}
+    #print(analizar_etiquetas(etiquetas_osm, etiquetas_esquema))
+
+    analizar_traza(ruta_gpx, debug)
